04c8221f2797bc5f5078a5270a1b74a6ad740d19
Konstantin Davydov <davydovks@mail.ru>
2023-11-15 17:34:57 +0500

    update link to homepage

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="bg-white border-gray-200 py-2.5 dark:bg-gray-900 shadow-md">
     <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
         <!--Title-->
-        <a href="https://php-task-manager-ru.hexlet.app" class="flex items-center">
+        <a href="{{ route('home') }}" class="flex items-center">
             <span
                 class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">{{ config('app.name', 'Менеджер задач') }}</span>
         </a>
