b69c71880bace40df6acd1e7ad7ce69d740761c5
Konstantin Davydov <davydovks@mail.ru>
2023-11-16 16:55:57 +0500

    add translations to views.auth section

--- lang/ru/views.php
+++ lang/ru/views.php
@@ -8,5 +8,9 @@ return [
         "confirm_password"=> "Подтверждение",
         "already_registered"=> "Уже зарегистрированы?",
         "register"=> "Зарегистрировать",
+        "remember_me"=> "Запомнить меня",
+        "forgot_your_password"=> "Забыли пароль?",
+        "log_in"=> "Войти",
+        "email_password_reset_link"=> "Сбросить пароль",
     ],
 ];
