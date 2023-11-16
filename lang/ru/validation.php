3ca0525f15bf3cf1831c6c07fc6a7410709a5f73
Konstantin Davydov <davydovks@mail.ru>
2023-11-16 17:58:02 +0500

    add translations for password verification

--- lang/ru/validation.php
+++ lang/ru/validation.php
@@ -2,4 +2,11 @@
 
 return [
     "something_went_wrong"=> "Упс! Что-то пошло не так:",
+    "attributes"=> [
+        "password"=> "пароль",
+    ],
+    "confirmed"=> ":Attribute и подтверждение не совпадают",
+    "min"=> [
+        "string"=> ":Attribute должен иметь длину не менее :min символов",
+    ],
 ];
