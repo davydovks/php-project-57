2a6d5ec5c80b0f8cddabc6a8d7f92def4082c99e
Konstantin Davydov <davydovks@mail.ru>
2023-11-16 21:20:51 +0500

    link task_statuses.create view texts to lang

--- resources/views/task_status/create.blade.php
+++ resources/views/task_status/create.blade.php
@@ -2,20 +2,20 @@
 
 @section('content')
     <div class="grid col-span-full">
-        <h1 class="mb-5">Создать статус</h1>
+        <h1 class="mb-5">{{ __('views.task_status.create.header') }}</h1>
 
         <form method="POST" action="{{ route('task_statuses.store') }}" accept-charset="UTF-8" class="w-50"><input
                 name="_token" type="hidden" value="{{ csrf_token() }}">
             <div class="flex flex-col">
                 <div>
-                    <label for="name">Имя</label>
+                    <label for="name">{{ __('views.task_status.create.label') }}</label>
                 </div>
                 <div class="mt-2">
                     <input class="rounded border-gray-300 w-1/3" name="name" type="text" id="name">
                 </div>
                 <div class="mt-2">
                     <input class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit"
-                        value="Создать">
+                        value="{{ __('views.task_status.create.button') }}">
                 </div>
             </div>
         </form>
