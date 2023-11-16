f93e18fc9369ad09e59cb9a51b052b4a3ea3a80b
Konstantin Davydov <davydovks@mail.ru>
2023-11-16 20:52:41 +0500

    link task_status layout labels to lang

--- resources/views/task_status/index.blade.php
+++ resources/views/task_status/index.blade.php
@@ -2,22 +2,22 @@
 
 @section('content')
     <div class="grid col-span-full">
-        <h1 class="mb-5">Статусы</h1>
+        <h1 class="mb-5">{{ __('views.task_status.index.header') }}</h1>
         @auth
             <div>
                 <a href="{{ route('task_statuses.create') }}"
                     class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Создать статус
+                    {{ __('views.task_status.index.create') }}
                 </a>
             </div>
         @endauth
         <table class="mt-4">
             <thead class="border-b-2 border-solid border-black text-left">
                 <tr>
-                    <th>ID</th>
-                    <th>Имя</th>
-                    <th>Дата создания</th>
-                    @auth<th>Действия</th>@endauth
+                    <th>{{ __('views.task_status.index.id') }}</th>
+                    <th>{{ __('views.task_status.index.name') }}</th>
+                    <th>{{ __('views.task_status.index.created_at') }}</th>
+                    @auth<th>{{ __('views.task_status.index.actions') }}</th>@endauth
                 </tr>
             </thead>
             <tbody>
@@ -28,13 +28,14 @@
                         <td>{{ $status->created_at }}</td>
                         @auth
                             <td>
-                                <a data-confirm="Вы уверены?" data-method="delete" class="text-red-600 hover:text-red-900"
-                                    href="https://php-task-manager-ru.hexlet.app/task_statuses/1">
-                                    Удалить
+                                <a data-confirm="__('views.task_status.index.delete_confirm')" data-method="delete"
+                                    class="text-red-600 hover:text-red-900"
+                                    href="{{ route('task_statuses.destroy', $status->id) }}">
+                                    {{ __('views.task_status.index.delete') }}
                                 </a>
                                 <a class="text-blue-600 hover:text-blue-900"
-                                    href="https://php-task-manager-ru.hexlet.app/task_statuses/1/edit">
-                                    Изменить
+                                    href="{{ route('task_statuses.edit', $status->id) }}">
+                                    {{ __('views.task_status.index.edit') }}
                                 </a>
                             </td>
                         @endauth
