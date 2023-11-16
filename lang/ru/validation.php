2e6adeeec7691f73d843fd92000ea398209742bc
Konstantin Davydov <davydovks@mail.ru>
2023-11-16 18:07:55 +0500

    add email validation translation

--- lang/ru/validation.php
+++ lang/ru/validation.php
@@ -3,10 +3,12 @@
 return [
     "something_went_wrong"=> "Упс! Что-то пошло не так:",
     "attributes"=> [
+        "email"=> "Email",
         "password"=> "пароль",
     ],
     "confirmed"=> ":Attribute и подтверждение не совпадают",
     "min"=> [
         "string"=> ":Attribute должен иметь длину не менее :min символов",
     ],
+    "unique" => "Такой :attribute уже используется",
 ];
