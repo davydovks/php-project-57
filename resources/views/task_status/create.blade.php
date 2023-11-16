28d66a4baa546d5801ab5acc4ede3eb52980c988
Konstantin Davydov <davydovks@mail.ru>
2023-11-16 23:32:10 +0500

    change form elements to laravelcollective

--- resources/views/task_status/create.blade.php
+++ resources/views/task_status/create.blade.php
@@ -4,20 +4,18 @@
     <div class="grid col-span-full">
         <h1 class="mb-5">{{ __('views.task_status.create.header') }}</h1>
 
-        <form method="POST" action="{{ route('task_statuses.store') }}" accept-charset="UTF-8" class="w-50"><input
-                name="_token" type="hidden" value="{{ csrf_token() }}">
+        {{ Form::open(['route' => 'task_statuses.store', 'method' => 'POST', 'class' => 'w-50']) }}
             <div class="flex flex-col">
                 <div>
-                    <label for="name">{{ __('views.task_status.create.label') }}</label>
+                    {{ Form::label('name', __('views.task_status.create.label')) }}
                 </div>
                 <div class="mt-2">
-                    <input class="rounded border-gray-300 w-1/3" name="name" type="text" id="name">
+                    {{ Form::text('name', null, ['class' => 'rounded border-gray-300 w-1/3']) }}
                 </div>
                 <div class="mt-2">
-                    <input class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit"
-                        value="{{ __('views.task_status.create.button') }}">
+                    {{ Form::submit(__('views.task_status.create.button'), ['class' => 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded']) }}
                 </div>
             </div>
-        </form>
+        {{ Form::close() }}
     </div>
 @endsection
