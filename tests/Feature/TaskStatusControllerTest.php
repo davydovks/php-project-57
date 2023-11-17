a4c3af2fa722486096890725995fa7ffd2559059
Konstantin Davydov <davydovks@mail.ru>
2023-11-17 15:13:12 +0500

    simplify use section

--- tests/Feature/TaskStatusControllerTest.php
+++ tests/Feature/TaskStatusControllerTest.php
@@ -3,10 +3,8 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use App\Models\{
-    User,
-    TaskStatus
-};
+use App\Models\User;
+use App\Models\TaskStatus;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TaskStatusControllerTest extends TestCase
