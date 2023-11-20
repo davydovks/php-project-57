6a50b27c21d6f27b5f21507b29812413950e5fdc
Konstantin Davydov <davydovks@mail.ru>
2023-11-20 21:19:15 +0500

    create TaskController

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Models\Task;
+use App\Models\TaskStatus;
+use App\Models\User;
+
+class TaskController extends Controller
+{
+    public function __construct()
+    {
+        $this->authorizeResource(Task::class);
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = Task::orderBy('id')->paginate();
+        return view('task.index', compact('tasks'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $taskStatuses = TaskStatus::all();
+        $users = User::all();
+        return view('task.create', compact('taskStatuses', 'users'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTaskRequest $request)
+    {
+        $data = $request->validated();
+        $task = new Task($data);
+        $task->save();
+
+        flash(__('flash.tasks.store.success'))->success();
+
+        return redirect()->route('tasks.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        return view('task.show', compact('task'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Task $task)
+    {
+        $taskStatuses = TaskStatus::all();
+        $users = User::all();
+        return view('task.edit', compact('task', 'taskStatuses', 'users'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        $data = $request->validated();
+        $task->fill($data);
+        $task->save();
+
+        flash(__('flash.tasks.update.success'))->success();
+
+        return redirect()->route('tasks.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        flash(__('flash.tasks.delete.success'))->success();
+        return redirect()->route('tasks.index');
+    }
+}
