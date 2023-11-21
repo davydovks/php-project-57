d0ecd4bb6e587a89c911d52b9a784ab1f92ae31e
Konstantin Davydov <davydovks@mail.ru>
2023-11-21 12:50:27 +0500

    update link to tasks.index page

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -35,7 +35,7 @@
         <div class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1">
             <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                 <li>
-                    <a href="{{ route('task_statuses.index') }}"
+                    <a href="{{ route('tasks.index') }}"
                         class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
                         {{ __('header.tasks') }}
                     </a>
