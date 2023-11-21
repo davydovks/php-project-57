a23d0bdf53043a0c7034495b05d188cd1cb1c3ad
Konstantin Davydov <davydovks@mail.ru>
2023-11-21 17:33:12 +0500

    change create login, logout and register buttons to link-button component

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -9,25 +9,11 @@
         <!--Buttons-->
         <div class="flex items-center lg:order-2">
             @auth
-                <a href="{{ route('logout') }}"
-                    onclick="event.preventDefault();
-                    document.getElementById('logout-form').submit();"
-                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">
-                    {{ __('header.logout') }}
-                </a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                    style="display: none;">
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                </form>
+                <x-link-button route="{{ route('logout') }}" text="{{ __('header.logout') }}" class="ml-2"
+                    data-method="POST" />
             @else
-                <a href="{{ route('login') }}"
-                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    {{ __('header.login') }}
-                </a>
-                <a href="{{ route('register') }}"
-                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">
-                    {{ __('header.register') }}
-                </a>
+                <x-link-button route="{{ route('login') }}" text="{{ __('header.login') }}" />
+                <x-link-button route="{{ route('register') }}" text="{{ __('header.register') }}" class="ml-2" />
             @endauth
         </div>
 
