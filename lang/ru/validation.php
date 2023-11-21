623c8c12757d73a4c38d10a14799b362e69361c6
Konstantin Davydov <davydovks@mail.ru>
2023-11-21 15:56:50 +0500

    add text for string rule in validation

--- lang/ru/validation.php
+++ lang/ru/validation.php
@@ -16,6 +16,7 @@ return [
         'string' => ':Attribute должен иметь длину не менее :min символов',
     ],
     'required' => 'Это обязательное поле',
+    'string' => 'Поле :attribute должно содержать строку',
     'unique' => 'Такой :attribute уже используется',
     'unique_name' => ':Attribute с таким именем уже существует',
 ];
