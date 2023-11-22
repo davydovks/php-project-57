608058eb985bfa647d99dc0886ee15d6cbc002b8
Konstantin Davydov <davydovks@mail.ru>
2023-11-22 12:02:58 +0500

    add createdTasks() method

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function createdTasks()
+    {
+        return $this->hasMany(Task::class, 'created_by_id');
+    }
 }
