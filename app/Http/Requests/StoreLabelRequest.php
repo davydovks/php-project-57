42b0012fe88fa8d31b062cb84ee64caf2e4655bb
Konstantin Davydov <davydovks@mail.ru>
2023-11-23 23:28:14 +0500

    create StoreLabelRequest

---
+++ app/Http/Requests/StoreLabelRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreLabelRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'max:255', Rule::unique('labels', 'name')->ignore($this->label)],
+            'description' => ['nullable', 'max:500'],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'name.unique' => __('validation.unique_name'),
+            'name.max' => __('validation.max.string_f'),
+            'description.max' => __('validation.max.string_n'),
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array<string, string>
+     */
+    public function attributes(): array
+    {
+        return [
+            'name' => __('validation.attributes.label'),
+            'description' => __('validation.attributes.description'),
+        ];
+    }
+}
