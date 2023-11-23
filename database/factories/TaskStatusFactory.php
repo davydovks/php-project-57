d42f50000acacda8f3aaa7fecf1bcdc9f9298123
Konstantin Davydov <davydovks@mail.ru>
2023-11-23 19:48:43 +0500

    change status name in TaskStatusFactory from name() to word()

--- database/factories/TaskStatusFactory.php
+++ database/factories/TaskStatusFactory.php
@@ -17,7 +17,7 @@ class TaskStatusFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'name' => fake()->word(),
             'created_at' => now(),
             'updated_at' => now(),
         ];
