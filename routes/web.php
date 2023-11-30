2ffefd86588fe289fe78201f7df158a779b5e98d
Konstantin Davydov <davydovks@mail.ru>
2023-11-30 19:11:50 +0500

    add spaces around .

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 Route::resource('task_statuses', TaskStatusController::class);
 Route::resource('tasks', TaskController::class);
