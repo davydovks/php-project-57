969d23fa98a296e9d0cdc41e5be405ad2ab23cb5
Konstantin Davydov <davydovks@mail.ru>
2023-11-30 18:40:49 +0500

    fix typo in words method

--- database/factories/TaskStatusFactory.php
+++ database/factories/TaskStatusFactory.php
@@ -17,7 +17,7 @@ class TaskStatusFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->unique()->word(3, true),
+            'name' => fake()->unique()->words(3, true),
             'created_at' => now(),
             'updated_at' => now(),
         ];
