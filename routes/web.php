ee476fc0742bfe02a561a4942c3702294827632e
Konstantin Davydov <davydovks@mail.ru>
2023-12-04 17:42:18 +0500

    delete profile routes, views and controller

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,6 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 require __DIR__ . '/auth.php';
 
 Route::resource('task_statuses', TaskStatusController::class);
